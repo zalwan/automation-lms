c0a9fb7a15cefa68f9282083e4ad95e11ddb1334
zalwan <rizalsuryawan86@gmail.com>
2026-01-14 23:21:19 +0700

    feat: add menu quisoner Exam

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
 	import Questionnaire from './questionnaire.svelte';
-    import MultipleChoice from './multipleChoice.svelte';
+	import ExamQuestionnaire from './examQuestionnaire.svelte';
+	import MultipleChoice from './multipleChoice.svelte';
 	import MessageAI from './messageAssistant.svelte';
 </script>
 
 <div class="mx-auto flex h-full max-w-xl flex-col gap-6">
 	<section class="grid gap-4">
 		<Questionnaire />
+		<ExamQuestionnaire />
 		<MultipleChoice />
 		<MessageAI />
 	</section>
